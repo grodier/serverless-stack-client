ae0823c32e109f3ac895fa1423a0187883372a0e
grodier <grodier3@gmail.com>
2020-04-26 17:52:36 -0400

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-2",
+      BUCKET: "gr-notes-app-uploads"
+    },
+    apiGateway: {
+      REGION: "us-east-2",
+      URL: "https://y8gf5yep46.execute-api.us-east-2.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-2",
+      USER_POOL_ID: "us-east-2_X1ZXsLQ8J",
+      APP_CLIENT_ID: "30k24gfotu66r3i0ufo7levnmr",
+      IDENTITY_POOL_ID: "us-east-2:78af062d-9888-40c7-92d9-a377724ea135"
+    }
+  };
